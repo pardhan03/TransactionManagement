fde3c15f2d74e6cbf2441458a6c809a5d67ed7e0
Manish <pardhanmanish7887@gmail.com>
2025-01-05 17:53:20 +0530

    hot fixes

--- db/connectMongoDB.js
+++ db/connectMongoDB.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URL || "mongodb://localhost:27017/Assignment"
+      process.env.MONGO_URL ||
+        "mongodb+srv://pardhanmanish7887:jBpVeZtxbeqDp9xJ@test-usertrail.c8xfj.mongodb.net/userTrail"
     );
     console.log(`MongoDB connected`);
   } catch (error) {
